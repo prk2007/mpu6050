f32dce8224f278e5450f5e27fa85a0de8abb9ee0
prk2007 <39105038+prk2007@users.noreply.github.com>
2022-02-07 17:00:02 +0530

    Update upcdn.js

--- upcdn.js
+++ upcdn.js
@@ -1,4 +1,5 @@
 console.log("dev-test-free" );
+console.log(window.parent.location);
 if (window.top.location.host != "localhost") {
     var antiClickjack = document.getElementById("antiClickjack");
     antiClickjack.parentNode.removeChild(antiClickjack);
