64ac1ab843bfe54698434a73205d88e92e7762ed
prk2007 <39105038+prk2007@users.noreply.github.com>
2022-02-07 16:15:15 +0530

    Update branding.js

--- branding.js
+++ branding.js
@@ -1,3 +1,4 @@
+console.log(window.parent.location );
 if (window.top.location.host != "localhost") {
     var antiClickjack = document.getElementById("antiClickjack");
     antiClickjack.parentNode.removeChild(antiClickjack);
