1edc0dbf125374dbd0a3b8af208425988eceba66
prk2007 <39105038+prk2007@users.noreply.github.com>
2022-02-07 16:11:22 +0530

    Update branding.js

--- branding.js
+++ branding.js
@@ -1,4 +1,4 @@
-if (window.top.location.host != "https://dev-test-free.pantheonsite.io/") {
+if (window.top.location.host != "localhost") {
     var antiClickjack = document.getElementById("antiClickjack");
     antiClickjack.parentNode.removeChild(antiClickjack);
 } else {
